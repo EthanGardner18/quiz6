200aa6b23a3613bab17eef3f52b3759e8df92de6
EthanGardner18 <ethangrdnr@gmail.com>
2024-03-01 21:33:58 -0600

    d.py done

--- d.py
+++ d.py
@@ -7,7 +7,7 @@ class Logger(ABC):
 
 class LoggingLibrary(Logger):
     def log(self, message):
-        # Implementation using a specific logging library like logging or loguru
+        print("Logging to library")
         pass
 
 class ConsoleLogger(Logger):
@@ -30,12 +30,10 @@ class LogManager:
         self.logger.log(message)
 
 if __name__ == "__main__":
-    # Example usage
     logging_library_logger = LoggingLibrary()
     console_logger = ConsoleLogger()
     file_logger = FileLogger('log.txt')
 
-    # Injecting different loggers into the LogManager
     log_manager = LogManager(logging_library_logger)
     log_manager.log_message("This is a log message using the logging library")
 
